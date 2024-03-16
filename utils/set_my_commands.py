5483ccde56d8a9ab984dd740f3f323f305ab0415
Boburbro <boburotaboyev0@gmail.com>
2024-03-16 17:54:25 +0500

    3- va 4- darslar qo'shildi

--- utils/set_my_commands.py
+++ utils/set_my_commands.py
@@ -4,7 +4,8 @@ import asyncio
 
 async def set_my_commands():
     await bot.set_my_commands([
-        BotCommand('start', 'Botni qayta ishga tushirish'),
+        BotCommand('start', 'Botni ishga tushirish'),
+        BotCommand('restart', 'Botni qayta ishga tushirish'),
         BotCommand('help', 'Yordam'),
     ])
 
